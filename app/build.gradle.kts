c1ce326a6c3239ac62c6350c353852a91fabe01b
subhra-io <subhrajeetpandey2001@gmail.com>
2026-06-12 12:45:26 +0530

    version upg for remote deploy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "gov.uidai.securemdmpoc"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.19"
+        versionCode = 21
+        versionName = "1.0.20"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
