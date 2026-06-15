a0ae06d2f62a4236979bf3d2590149f2eba35f85
subhra-io <subhrajeetpandey2001@gmail.com>
2026-06-15 11:25:11 +0530

    enum class

---
+++ app/src/main/java/gov/uidai/securemdmpoc/util/enums.kt
@@ -0,0 +1,17 @@
+package gov.uidai.securemdmpoc.util
+
+enum class AppCategory {
+    OUR_APP,
+    ESSENTIAL,
+    COMMUNICATION,
+    MEDIA,
+    PRODUCTIVITY,
+    CAMERA_DEDICATED,
+    CAMERA_CAPABLE,
+    BROWSER,
+    STORE,
+    ASSISTANT,
+    SYSTEM_UI,
+    SAFE_UTILITY,
+    UNKNOWN
+}
